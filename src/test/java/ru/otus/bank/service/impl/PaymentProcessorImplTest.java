91807c8b6943666a1e83fdf69d79ccd2d394075f
Ilya Cherney <ilyacherney@yandex.ru>
2024-04-22 10:25:21 +0300

    new unit tests added

--- src/test/java/ru/otus/bank/service/impl/PaymentProcessorImplTest.java
+++ src/test/java/ru/otus/bank/service/impl/PaymentProcessorImplTest.java
@@ -1,18 +1,25 @@
 package ru.otus.bank.service.impl;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatcher;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import ru.otus.bank.dao.AccountDao;
 import ru.otus.bank.entity.Account;
 import ru.otus.bank.entity.Agreement;
 import ru.otus.bank.service.AccountService;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.argThat;
 import static org.mockito.Mockito.when;
 
@@ -24,6 +31,9 @@ public class PaymentProcessorImplTest {
 
     @InjectMocks
     PaymentProcessorImpl paymentProcessor;
+
+    @Mock
+    AccountDao accountDao;
 
     @Test
     public void testTransfer() {
@@ -55,9 +65,58 @@ public class PaymentProcessorImplTest {
             }
         }))).thenReturn(List.of(destinationAccount));
 
+        when(accountDao.findById(eq(1L))).thenReturn(Optional.of(sourceAccount));
+
         paymentProcessor.makeTransfer(sourceAgreement, destinationAgreement,
                 0, 0, BigDecimal.ONE);
 
+    }
+
+    @Test
+    public void testMakeTransferWithComission() {
+        Account sourceAccount = new Account();
+        sourceAccount.setAmount(new BigDecimal(200));
+        sourceAccount.setType(0);
+        sourceAccount.setId(1L);
+
+        Account destinationAccount = new Account();
+        destinationAccount.setAmount(new BigDecimal(300));
+        destinationAccount.setType(0);
+        destinationAccount.setId(2L);
+
+        Agreement sourceAgreement = new Agreement();
+        sourceAgreement.setId(1L);
+
+        Agreement destinationAgreement = new Agreement();
+        destinationAgreement.setId(2L);
+
+        when(accountService.getAccounts(argThat(new ArgumentMatcher<Agreement>() {
+            @Override
+            public boolean matches(Agreement argument) {
+                return argument != null && argument.getId() == 1L;
+            }
+        }))).thenReturn(List.of(sourceAccount));
+
+        when(accountService.getAccounts(argThat(new ArgumentMatcher<Agreement>() {
+            @Override
+            public boolean matches(Agreement argument) {
+                return argument != null && argument.getId() == 2L;
+            }
+        }))).thenReturn(List.of(destinationAccount));
+
+        when(accountDao.findById(eq(1L))).thenReturn(Optional.of(sourceAccount));
+        when(accountDao.findById(eq(2L))).thenReturn(Optional.of(destinationAccount));
+
+
+
+        Mockito.when(accountDao.save(sourceAccount)).thenReturn(sourceAccount);
+        Mockito.when(accountDao.save(destinationAccount)).thenReturn(destinationAccount);
+
+        assertTrue(paymentProcessor.makeTransferWithComission(sourceAgreement, destinationAgreement,
+                0, 0, new BigDecimal(50), new BigDecimal(10)));
+
+        Assertions.assertEquals(new BigDecimal(145), sourceAccount.getAmount());
+        Assertions.assertEquals(new BigDecimal(350), destinationAccount.getAmount());
     }
 
 }
